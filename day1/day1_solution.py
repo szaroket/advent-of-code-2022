c305ae3cc4fbf752f83fd9fe1d322db8c920fb59
szaroket <30178095+szaroket@users.noreply.github.com>
2022-12-01 15:23:18 +0100

    Add solution to the second part of Day 1

--- day1/day1_solution.py
+++ day1/day1_solution.py
@@ -2,6 +2,8 @@
 # Day 1: Calorie Counting
 # https://adventofcode.com/2022/day/1
 
+TOP_NUMBER_OF_ELVES = 3
+
 
 def read_input() -> list:
     with open("input.txt", "r") as f:
@@ -13,12 +15,25 @@ def prepare_data(data: list) -> list:
     return [list(map(int, elf.split("\n"))) for elf in data.split("\n\n")]
 
 
-def get_max_calories(data: list) -> list:
+def get_max_calories(data: list) -> int:
     return sum(max(data, key=sum))
+
+
+def sum_and_sort_calories(data: list) -> list:
+    return sorted([sum(elf) for elf in data], reverse=True)
+
+
+def sum_top_three_calories(data: list) -> int:
+    return sum(data[0:TOP_NUMBER_OF_ELVES])
 
 
 if __name__ == "__main__":
     input_data = read_input()
     prepared_data = prepare_data(input_data)
     max_calories = get_max_calories(prepared_data)
-    print(f"The highest number of calories carried by an elf: {max_calories}")
+    print(f"The highest number of calories carried by the Elf: {max_calories}")
+    sorted_total_calories = sum_and_sort_calories(prepared_data)
+    top_three_sum = sum_top_three_calories(sorted_total_calories)
+    print(
+        f"The highest number of calories carried by the top three Elves: {top_three_sum}"
+    )
