6279d127c3c5b7de07049b6a2682f3c1923af967
szaroket <30178095+szaroket@users.noreply.github.com>
2022-12-06 08:19:16 +0100

    Add solution to part 2 of Day 6

--- day6/day6_solution.py
+++ day6/day6_solution.py
@@ -1,7 +1,8 @@
 # Advent of code 2022
 # Day 6: Tuning Trouble
 # https://adventofcode.com/2022/day/6
-MIN_LENGTH = 4
+MIN_LENGTH_PART1 = 4
+MIN_LENGTH_PART2 = 14
 
 
 def read_input() -> str:
@@ -14,10 +15,10 @@ def prepare_data(data: str) -> list:
     return [sign for sign in data]
 
 
-def check_marker_index(data: list) -> int:
+def check_marker_index(data: list, min_length: int) -> int:
     start_point = 0
-    for index in range(4, len(data)):
-        if len(set(data[start_point:index])) == MIN_LENGTH:
+    for index in range(min_length, len(data)):
+        if len(set(data[start_point:index])) == min_length:
             return index
         start_point += 1
 
@@ -25,5 +26,7 @@ def check_marker_index(data: list) -> int:
 if __name__ == "__main__":
     input_data = read_input()
     prepared_data = prepare_data(input_data)
-    marker = check_marker_index(prepared_data)
-    print(f"The first start-of-packet marker is detected on position: {marker}")
+    marker1 = check_marker_index(prepared_data, MIN_LENGTH_PART1)
+    print(f"The first start-of-packet marker is detected on position: {marker1}")
+    marker2 = check_marker_index(prepared_data, MIN_LENGTH_PART2)
+    print(f"The first start-of-packet marker is detected on position: {marker2}")
