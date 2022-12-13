b30629f04500bf42ca9611efd3622fa160de317d
szaroket <30178095+szaroket@users.noreply.github.com>
2022-12-13 15:15:32 +0100

    Add solution to part 2 of Day 13

--- day13/day13_solution.py
+++ day13/day13_solution.py
@@ -1,6 +1,11 @@
 # Advent of code 2022
 # Day 13: Distress Signal
 # https://adventofcode.com/2022/day/13
+from functools import cmp_to_key
+from typing import Any, Type
+
+DIVIDER1 = [[[2]]]
+DIVIDER2 = [[[6]]]
 
 
 def read_input() -> str:
@@ -11,6 +16,10 @@ def read_input() -> str:
 
 def prepare_data(data: str) -> list[list]:
     return [list(map(eval, x.split("\n"))) for x in data.split("\n\n")]
+
+
+def prepare_data2(data: str) -> list[Type[list[Any]]]:
+    return list(map(eval, data.strip().replace("\n\n", "\n").split("\n")))
 
 
 def compare_values(left, right):
@@ -56,7 +65,21 @@ def get_solution_part1(data: list[list]):
     print(f"The sum of the indices of pairs: {solution}")
 
 
+def get_solution_part2(data: list[list]):
+    data.append(DIVIDER1)
+    data.append(DIVIDER2)
+    lists = sorted(data, key=cmp_to_key(compare_values), reverse=True)
+    for index, signal in enumerate(lists):
+        if signal == DIVIDER1:
+            a = index + 1
+        if signal == DIVIDER2:
+            b = index + 1
+    print(f"The decoder key for the distress signal: {a * b}")
+
+
 if __name__ == "__main__":
     input_data = read_input()
     prepared_data = prepare_data(input_data)
     get_solution_part1(prepared_data)
+    prepared_data2 = prepare_data2(input_data)
+    get_solution_part2(prepared_data2)
